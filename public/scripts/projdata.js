d08d944baea9e9a3821eb7ea79d80bcdcd7d9dc6
aileen santos <asantos3026@gmail.com>
2015-07-23 16:00:09 -0700

    added files

--- public/scripts/projdata.js
+++ public/scripts/projdata.js
@@ -7,6 +7,30 @@ $(document).on("ready", function () {
     // compile blog template
   var template = _.template($('#projects-template').html());
 
+    L.mapbox.accessToken = 'pk.eyJ1IjoiYXNhbnRvczMwMjYiLCJhIjoiZWZlMmMyM2JiN2ZiNzcxZmJkOGJhMWNhZWE4ODc1MjMifQ.Moj73Bv5_uyylRIcZkXcYg';
+    var map = L.mapbox.map('map', 'mapbox.streets')
+                .setView([40, -74.50], 9);
+    var geocoder = L.mapbox.geocoder('mapbox.places');
+   //adds marker to the page based on geocoder lng and lat
+    var showMarker = function(address, lng, lat) {
+      L.mapbox.featureLayer({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [
+            lng,
+            lat
+          ]
+        },
+        properties: {
+          description: address,
+          'marker-size': 'small',
+          'marker-color': '#19B919',
+          // 'marker-symbol': 'flag'
+        }
+      }).addTo(map);
+    };
+
   //GET all archived project posts
   var all = function () {
     $.get('/api/projects', function(data) {
@@ -15,6 +39,21 @@ $(document).on("ready", function () {
       console.log(allProjects);
       //iterate through all projects
       _.each(allProjects, function(data) {
+        console.log(data)
+            //this is my address which is coming from an array of results in my api
+        var address = data.address;
+
+
+        //mapbox function to turn an address into  lat and long
+        geocoder.query(address, function(err, result) {
+            console.log('result.latlng[1]: ', result.latlng[1], '  result.latlng[0]: ', result.latlng[0]);
+             if (err) {
+               console.log(err);
+             }
+             else {
+               showMarker(address, result.latlng[1], result.latlng[0]);
+             }
+          });
 
         //pass each project through template to append to view 
         var $projectHtml = $(template({project: data}));
